174d527342708a1396fa9c8f0b00ba5712724afc
Sal Zamora <sal.zamora480@gmail.com>
2021-12-02 09:48:05 -0700

    added deps to packagejson

--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const AuthRouter = require('ROUTER NAME LOCATION HERE');
-const UsersRouter = require('ROUTER NAME LOCATION HERE');
-const RecipesRouter = require('ROUTER NAME LOCATION HERE');
+const authenticate = require('../auth/restricted-middleware');
+
+const AuthRouter = require('../routing/auth/auth-router');
+const UsersRouter = require('../routing/users/users-router');
+const RecipesRouter = require('../routing/recipes/recipes-router');
 
 const server = express();
-
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
 server.use('/api/auth', AuthRouter);
 server.use('/api/users', UsersRouter);
 server.use('/api/recipes', RecipesRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up' });
+});
 
 module.exports = server;
